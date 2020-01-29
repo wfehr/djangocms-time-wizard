f71a2e58604689bdfd8dc58485b966e2d05a2ca8
Wolfgang Fehr <dev@wfehr.de>
2020-01-29 13:19:06 +0100

    Bugfix: import TimeWizard-Mixins correctly

--- djangocms_time_wizard/models.py
+++ djangocms_time_wizard/models.py
@@ -1,9 +1,10 @@
 from cms.models import CMSPlugin
-from time_wizard.models import TimeWizardInlineMixin, TimeWizardMixin
+from time_wizard.mixins import TimeWizardInlineMixin, TimeWizardMixin
 
 
 class TimeWizardModel(TimeWizardMixin, CMSPlugin):
     pass
+
 
 class TimeWizardInlineModel(TimeWizardInlineMixin, CMSPlugin):
     pass
