21ca1dbaf548c3a4d6e0f7ae93fb78497839a8d6
Achim Zagler <achim.zagler@fotopuzzle.de>
2020-01-29 08:06:31 +0100

    Implemented TimeWizardInlinePlugin

--- djangocms_time_wizard/models.py
+++ djangocms_time_wizard/models.py
@@ -1,6 +1,9 @@
 from cms.models import CMSPlugin
-from time_wizard.models import TimeWizardMixin
+from time_wizard.models import TimeWizardInlineMixin, TimeWizardMixin
 
 
 class TimeWizardModel(TimeWizardMixin, CMSPlugin):
+    pass
+
+class TimeWizardInlineModel(TimeWizardInlineMixin, CMSPlugin):
     pass
